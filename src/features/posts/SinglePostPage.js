ac6b76f474fc8c994adb518ad46dc3b8c7fc041c
Brian Eyster <eysterbrian@gmail.com>
2020-10-05 18:16:51 -0600

    Adds component to display a single post
    - Adds route to App for /posts/:postId
    - Adds new SinglePostPage component.

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+// NOTE: This component expects the postId to be passed as
+// a match.params from React Router
+export default function SinglePostPage({ match }) {
+  const { postId } = useParams() // Access match.params.postId
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postId)
+  )
+  if (!post) {
+    return (
+      <section>
+        <h2>Sorry, that post does not exist!</h2>
+      </section>
+    )
+  }
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <p className="post-content">{post.content}</p>
+      </article>
+    </section>
+  )
+}
