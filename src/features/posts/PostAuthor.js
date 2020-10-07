0d948fa591fc5ffd64816a02d733f949db6cb4d7
Brian Eyster <eysterbrian@gmail.com>
2020-10-07 15:09:27 -0600

    Fixes bug with PostAuthor when author missing
    - Uses conditional chaining (?.) to deal with missing author

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -4,5 +4,5 @@ import { selectUserById } from '../users/usersSlice'
 
 export default function PostAuthor({ userId }) {
   const author = useSelector((state) => selectUserById(state, userId))
-  return <span>by {author.name ? author.name : 'Unknown author'} </span>
+  return <span>by {author?.name ? author.name : 'Unknown author'} </span>
 }
