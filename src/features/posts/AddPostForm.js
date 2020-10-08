de2f690c2ccf2fa6b2e41715c961cf6c4ffbfe4d
Brian Eyster <eysterbrian@gmail.com>
2020-10-08 08:52:45 -0600

    Saves newly created posts to API
    - postsSlice implements new createPost async thunk and an
    extraReducer for its fulfilled lifecycle action
    - Adds an addPostStatus to AddPostForm so that the form will display
    a saving state during the dispatched async createPost()

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,30 +1,51 @@
+import { unwrapResult } from '@reduxjs/toolkit'
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectAllUsers } from '../users/usersSlice'
-import { postAdded } from './postsSlice'
+import { createPost } from './postsSlice'
 
 export default function AddPostForm() {
   // Keep local state for managing form elements
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addPostStatus, setAddPostStatus] = useState('idle')
   const dispatch = useDispatch()
 
-  const onSavePostClicked = () => {
-    // Only save this post if it has a title and some content
-    if (title && content) {
-      dispatch(postAdded(title, content, userId))
+  const canSave =
+    [title, content, userId].every(Boolean) && addPostStatus === 'idle'
 
-      // Clear the form once the post has been saved
-      setTitle('')
-      setContent('')
-      setUserId('')
+  const onSavePostClicked = async () => {
+    // Only save this post if it has a title and some content
+    if (canSave) {
+      try {
+        setAddPostStatus('pending')
+
+        // createPost returns a Promise from dispatch
+        const resultAction = await dispatch(
+          createPost({ title, content, userId })
+        )
+        // The awaited promise will return the final action dispatched
+        // which could be either createPost.fulfilled or createPost.error.
+        // unwrapResult() will return either the payload from fulfilled or throw an error
+        unwrapResult(resultAction)
+
+        // Clear the form once the post has been saved
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (err) {
+        console.log('Failed to save the post', err)
+      } finally {
+        setAddPostStatus('idle')
+      }
     }
   }
 
   const users = useSelector(selectAllUsers)
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  if (addPostStatus !== 'idle') {
+    return <div className="loader">Saving post...</div>
+  }
 
   return (
     <section>
