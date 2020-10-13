30b63b267ee03c8d29b3088242b5b02db6f3898c
Brian Eyster <eysterbrian@gmail.com>
2020-10-13 15:49:00 -0600

    Displays num unread notifications in Navbar
    - adds span.badge with number of notifications with !read property

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fetchNotifications } from '../features/notifications/notificationsSlice'
+import {
+  fetchNotifications,
+  selectAllNotifications,
+} from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   // The 'Refresh Notifications' button will dispatch an API call
@@ -9,6 +12,11 @@ export const Navbar = () => {
   const fetchNewNotifications = () => {
     dispatch(fetchNotifications())
   }
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.reduce(
+    (accum, n) => accum + (!n.read ? 1 : 0),
+    0
+  )
 
   return (
     <nav>
@@ -19,7 +27,14 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
-            <Link to="/notifications">Notifications</Link>
+            <Link to="/notifications">
+              Notifications{' '}
+              {numUnreadNotifications > 0 ? (
+                <span className="badge">{numUnreadNotifications}</span>
+              ) : (
+                ''
+              )}
+            </Link>
           </div>
           <button type="button" onClick={fetchNewNotifications}>
             Refresh Notifications
